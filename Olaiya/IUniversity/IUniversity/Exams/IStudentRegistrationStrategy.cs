721584fc2f50b88f5eec930c1f9236093a9a24b8
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-04-25 13:59:38 +0200

    Add Student registration strategy interface

---
+++ Olaiya/IUniversity/IUniversity/Exams/IStudentRegistrationStrategy.cs
@@ -0,0 +1,12 @@
+﻿using IUniversity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IUniversity.Exams
+{
+    interface IStudentRegistrationStrategy
+    {
+        void register(IList<IStudent> list, IStudent student);
+    }
+}
