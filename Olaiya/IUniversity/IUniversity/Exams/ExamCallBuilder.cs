4a97d78c2df5f9f75b8dbd3029768e33df6b5ca9
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-04-25 17:10:15 +0200

    Add Exam call builder

---
+++ Olaiya/IUniversity/IUniversity/Exams/ExamCallBuilder.cs
@@ -0,0 +1,72 @@
+﻿using IUniversity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static IUniversity.Exams.IExamCall;
+
+namespace IUniversity.Exams
+{
+    class ExamCallBuilder
+    {
+        private const int DAYS_BEFORE_CALL = 1;
+        private int maximumStudents;
+        private DateTime callStart;
+        private ExamType examType;
+        private ICourse course;
+        private IStudentRegistrationStrategy registrationStrategy;
+        private bool built;
+
+        public ExamCallBuilder()
+        {
+            registrationStrategy = new StudentRegistrationFactory().AtTheEndOfList();
+        }
+
+        public ExamCallBuilder CallStart(DateTime callStart)
+        {
+            this.callStart = callStart;
+            return this;
+        }
+
+        public ExamCallBuilder ExamType(ExamType examType)
+        {
+            this.examType = examType;
+            return this;
+        }
+
+        public ExamCallBuilder Course(ICourse course)
+        {
+            this.course = course;
+            return this;
+        }
+
+        public ExamCallBuilder MaximumStudents(int maximumStudents)
+        {
+            this.maximumStudents = maximumStudents;
+            return this;
+        }
+
+        public ExamCallBuilder RegistrationStrategy(IStudentRegistrationStrategy registrationStrategy)
+        {
+            this.registrationStrategy = registrationStrategy;
+            return this;
+        }
+
+        public ExamCall Build()
+        {
+            if (built)
+            {
+                throw new InvalidOperationException("Builder already consumed");
+            }
+            else if (course is null)
+            {
+                throw new InvalidOperationException("Arguments missing");
+            }
+            else if (DateTime.Compare(DateTime.Now, DateTime.Now.AddDays(-DAYS_BEFORE_CALL)) >= 0)
+            {
+                throw new InvalidOperationException("Exam call creation is late");
+            }
+            built = true;
+            return new ExamCall(course, callStart, examType, maximumStudents, registrationStrategy);
+        }
+    }
+}
