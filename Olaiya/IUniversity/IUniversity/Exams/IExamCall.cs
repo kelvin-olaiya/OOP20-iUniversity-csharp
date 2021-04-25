eaf499fca759c3bcbb10d5cbf0e2dae7d7223c6f
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-04-25 12:49:18 +0200

    Add Exam call interface

---
+++ Olaiya/IUniversity/IUniversity/Exams/IExamCall.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using IUniversity.Entities;
+
+namespace IUniversity.Exams
+{
+    interface IExamCall
+    {
+        enum CallStatus { OPEN, CLOSED }
+        enum ExamType { ORAL, WRITTEN, PRACTICAL }
+
+        ICourse Course { get; }
+
+        ISet<IStudent> RegisteredStudents();
+
+        IList<IStudent> RegistrationList();
+
+        DateTime Start { get; }
+
+        ExamType Type { get; }
+
+        CallStatus Status();
+
+        int MaxStudents { get; }
+
+        bool RegisterStudent(IStudent student);
+
+        bool WithdrawStudent(IStudent student);
+
+        bool IsOpen();
+
+        bool IsFull();
+
+    }
+}
